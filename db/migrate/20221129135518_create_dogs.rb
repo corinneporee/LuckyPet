97e3f566f2f1b49d579129004daccd343f2708e1
Aurelie Becques <aurelie.becques@gmail.com>
2022-11-29 15:05:09 +0100

    create Dog model

---
+++ db/migrate/20221129135518_create_dogs.rb
@@ -0,0 +1,16 @@
+class CreateDogs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+      t.float :weight
+      t.date :date_of_birth
+      t.string :gender
+      t.string :health
+      t.boolean :neutered
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
