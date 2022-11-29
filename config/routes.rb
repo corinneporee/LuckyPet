0b85dce6afee3710fa9b67b9054dcc2632bd396b
steven dorion <noirod.steven@gmail.com>
2022-11-29 18:12:24 +0100

    create routes and controllers to review

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,26 @@
 Rails.application.routes.draw do
+  get 'walk/show'
+  get 'walk/create'
+  get 'friendship/index'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :dogs do
+    resources :friendships, only: %i[create]
+  end
+
+  resources :friendships, only: %i[index]
+
+  resources :spots do
+    resources :walks, only: %i[create]
+    resources :reviews, only: %i[create]
+  end
+
+  resources :walks, only: %i[show] do
+    resources :invitations, only: %i[create]
+  end
+
+  patch ":id/accept", to: "invitation#accept"
 
   # Defines the root path route ("/")
   # root "articles#index"
