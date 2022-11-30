9e3c44588690311ca0f4e3dafaefd4b3ce531e02
Aurelie Becques <aurelie.becques@gmail.com>
2022-11-30 17:58:32 +0100

    Seed suite

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,10 +1,22 @@
 class Spot < ApplicationRecord
+  SPOT_TYPES = ["Balades", "Commerces", "Vétérinaires", "Toiletteurs"]
+  WALK_ENVIRONMENTS = ["Forêt", "Plage", "Ville"]
+  WALK_AREA = ["Petite", "Moyenne", "Grands"]
+  WALK_ATTENDANCE = ["Faible", "Moyenne", "Forte"]
+
+  validates :walk_environment, inclusion: { in: WALK_ENVIRONMENTS, allow_blank: true }
+  validates :walk_area, inclusion: { in: WALK_AREA, allow_blank: true }
+  validates :walk_attendance, inclusion: { in: WALK_ATTENDANCE, allow_blank: true }
+  validates :spot_type, presence: true, inclusion: { in: SPOT_TYPES }
+
   belongs_to :user
   has_many :walks
   has_many :reviews
+  has_one_attached :photo
 
-  validates :name, :adress, :spot_type, :vet_specialty, presence: true
-  validates :walk_environment, :walk_size, :walk_area, :shop_vrac, presence: true
-  validates :shop_cereale, :shop_insects, :pension_pro, :pension_walks, :grooming_shop, presence: true
-  validates :description, :walk_attendance, allow_blank: true
+  validates :name, :adress, :spot_type, presence: true
+
+  #validates :walk_environment, :walk_attendance, :walk_area, :shop_vrac, presence: true
+  #validates :shop_cereale, :shop_insects, :pension_pro, :pension_walks, :grooming_shop, presence: true
+  #validates :description, :walk_attendance, allow_blank: true
 end
--- app/models/walk.rb
+++ app/models/walk.rb
@@ -1,4 +1,7 @@
 class Walk < ApplicationRecord
+
+
   belongs_to :spot
   belongs_to :dog
+
 end
