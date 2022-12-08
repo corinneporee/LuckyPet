085d580331d1290915fb7464cc93b3244d964941
Rodolphe Junker <rody@ancre-agence.com>
2022-12-08 09:12:34 +0100

    Styling map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/rrooddyy/clbc406me000615uonlk4kh6n",
+      style: "mapbox://styles/rrooddyy/clbc4g3a8001b15n6osi8l692",
     })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
